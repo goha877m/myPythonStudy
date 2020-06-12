cc3475eda0edcb29dbb88e34a8330adb426485bb
goha877m <jzy511@qq.com>
2020-06-12 21:44:21 +0800

    first commit

---
+++ python.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# a="name"
+# print(a)
+# a=10
+# print(a)
+# CONST = "DOND"
+# print(CONST)
+# CONST=10
+# print(CONST)
+# chr(36)
+# print('%.2f' % 3.1415926)
+# print('Hello, {0}, 成绩提升了 {1:.1f}%'.format('小明', 17.125))
+# 列表
+# lista = []
+# print(len(lista))
+# lista.append("1")
+# lista.append(2)
+# lista.append("min")
+# lista.append(3.14)
+# print(lista)
+# print(lista[-1])
+# # 元组
+# tuplea = (1,);
+# print(tuplea)
+# 条件
+# height = input("请输入身高cm:")
+# height = int(height)/100;
+# weight = input("请输入体重kg:")
+# weight = int(weight)
+# bmi = weight/(height**2)
+# if(bmi <18.5):
+#     print("过轻")
+# elif(bmi>=18.5 and bmi <25):
+#     print("正常")
+# elif(bmi>=25 and bmi <28):
+#     print("过重")
+# elif(bmi>=28 and bmi <32):
+#     print("肥胖")
+# else:
+#     print("严重肥胖")
